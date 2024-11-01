926865ecb441662f9601654b871267623ac07f5d
Dirk Eisold <deisold@gmail.com>
2024-11-01 16:50:19 +0100

    added frontend: added ComedyClashAdapter

---
+++ frontend/src/adapters/ComedyClashAdapter.js
@@ -0,0 +1,18 @@
+const ethers = require("ethers");
+import ComedyClashJSON from "../utils/ComedyClash.json"
+
+export const ComedyClashAdapter = (web3Provider, address) => {
+    let contract;
+
+    async function getContract(web3Provider, address) {
+        return contract || (contract = new ethers.Contract(
+            address,
+            ComedyClashJSON.abi,
+            web3Provider
+        ));
+    }
+
+    return {
+        getDescription: async () => (await getContract()).description()
+    }
+}
