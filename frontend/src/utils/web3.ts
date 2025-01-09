9fea5f8e28040b81a7635f403b1011d9ac911309
Dirk Eisold <deisold@gmail.com>
2025-01-09 17:41:41 +0100

    initWeb3Provider now using WalletConnect for mobile use case

--- frontend/src/utils/web3.ts
+++ frontend/src/utils/web3.ts
@@ -1,11 +1,12 @@
 import { ethers, Network, Signer } from "ethers";
 
+import { EthereumProvider } from "@walletconnect/ethereum-provider";
 /**
- * Initializes a Web3 provider using either MetaMask or Infura fallback
+ * Initializes a Web3 z using either MetaMask or Infura fallback
  * @returns {Promise<ethers.Provider>} The initialized provider
  */
 export async function initWeb3Provider(): Promise<ethers.Provider> {
-    let provider: ethers.Provider;
+    let provider: ethers.Provider | null = null;
 
     console.log(`initWeb3Provider: INFURA_ENDPOINT=${process.env.NEXT_PUBLIC_INFURA_ENDPOINT}`);
 
@@ -18,13 +19,31 @@ export async function initWeb3Provider(): Promise<ethers.Provider> {
             // Request account access
             await (window as any).ethereum.request({ method: 'eth_requestAccounts' });
         } else {
+
+            try {
+                const sepoliaTestnet = 11155111;
+                const wcProvider = await EthereumProvider.init({
+                    projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID as string,
+                    chains: [1, sepoliaTestnet], // mainnet and sepoliaTestnet
+                    showQrModal: true
+                });
+                console.log("Enabling provider...");
+                await wcProvider.enable();
+                console.log("Provider enabled successfully");
+
+                provider = new ethers.BrowserProvider(wcProvider);
+            } catch (error) {
+                console.log(`initWeb3Provider: error using WalletConnect`);
+            }
+
             // Fallback to read-only JsonRpcProvider
             console.log("initWeb3Provider: using JsonRpcProvider(infura)");
-
-            provider = new ethers.JsonRpcProvider(process.env.NEXT_PUBLIC_INFURA_ENDPOINT as string);
+            if (!provider) {
+                provider = new ethers.JsonRpcProvider(process.env.NEXT_PUBLIC_INFURA_ENDPOINT as string);
+            }
         }
 
-        console.log('initWeb3Provider: Provider initialized successfully');
+        console.log(`initWeb3Provider: Provider initialized successfully: provider=${provider}`);
         return provider;
     } catch (error) {
         console.error('initWeb3Provider: Failed to initialize provider', error);
