0c308478c10918ab9b1f1b25a9a9b4622a4a06b1
Dirk Eisold <deisold@gmail.com>
2025-02-28 20:38:08 +0100

    frontend: added generateRandomHash

--- frontend/src/utils/utils.ts
+++ frontend/src/utils/utils.ts
@@ -6,3 +6,13 @@ export function isNotNullOrEmpty(str: string | null | undefined): boolean {
 
 export const hasNoErrors = <T extends BaseErrorMessage>(errors: T): boolean =>
   Object.values(errors).every(value => value === '');
+
+export function generateRandomHash(): string {
+  const length = 64; // 32 bytes in hexadecimal
+  const characters = 'abcdef0123456789';
+  let result = '0x';
+  for (let i = 0; i < length; i++) {
+      result += characters[Math.floor(Math.random() * characters.length)];
+  }
+  return result;
+}
