a97a92e50c3900363e7c31e483d67daf90d2f334
Dirk Eisold <deisold@gmail.com>
2025-02-28 20:40:17 +0100

    frontend: MockComedyTheaterAdapter: generating random hash for show address

--- frontend/src/adapters/MockComedyTheaterAdapter.ts
+++ frontend/src/adapters/MockComedyTheaterAdapter.ts
@@ -1,7 +1,8 @@
 // frontend/src/adapters/MockComedyTheaterAdapter.ts
 import { ComedyTheaterAdapterType } from "./ComedyTheaterAdapterType";
 import { createDelayedMockResponse as createDelayedMockResponse, defaultDelayMS } from "./MockTransactionResponse";
-
+import { generateRandomHash } from "../utils/utils";
+//
 const delay = (ms: number) => {
     return new Promise((resolve) => setTimeout(resolve, ms));
 };
@@ -10,7 +11,7 @@ export const MockComedyTheaterAdapter = (): ComedyTheaterAdapterType => {
     let amount: bigint = BigInt(5);
     return {
         getShowAmount: async () => amount,
-        getShowAdress: async (index: number) => "0x0e6877867689768",
+        getShowAdress: async (index: number) => generateRandomHash(),
         // getShowAdress: async (index: number) => index.toString(),
         addShow: async (description: string, durationInDays: number) => {
             await delay(defaultDelayMS);
