6374e5e459fdcdf1ae8e069da2ee11d75c1eced0
Dirk Eisold <deisold@gmail.com>
2025-03-03 23:02:25 +0100

    frontend: added comments show store

--- frontend/src/store/ShowStore.ts
+++ frontend/src/store/ShowStore.ts
@@ -27,10 +27,16 @@ export const useShowStore = create<ShowStoreType>((set, get) => ({
         const showEntry = get().showMapping.find((userMapping) => userMapping.index === index);
         return showEntry ? showEntry.data : null;
     },
-    updateShowForIndex: (index: number, show: Partial<Show>) => set((state) => ({
-        showMapping: state.showMapping.map((userMapping) => userMapping.index === index ? { ...userMapping, data: { ...userMapping.data, ...show } } : userMapping)
-    })),
-    updateShowForTxHash: (txHash: string, show: Partial<Show>) => set((state) => ({
-        showMapping: state.showMapping.map((userMapping) => userMapping.address === txHash ? { ...userMapping, data: { ...userMapping.data, ...show } } : userMapping)
-    })),
+    updateShowForIndex: (index: number, show: Partial<Show>) => set((state) => {
+        console.log(`ShowStore::updateShowForIndex index=${index} update=${JSON.stringify(show)}`);
+        return {
+            showMapping: state.showMapping.map((userMapping) => userMapping.index === index ? { ...userMapping, data: { ...userMapping.data, ...show } } : userMapping)
+        }
+    }),
+    updateShowForTxHash: (txHash: string, show: Partial<Show>) => set((state) => {
+        console.log(`ShowStore::updateShowForTxHash txHash=${txHash} update=${JSON.stringify(show)}`);
+        return {
+            showMapping: state.showMapping.map((userMapping) => userMapping.address === txHash ? { ...userMapping, data: { ...userMapping.data, ...show } } : userMapping)
+        }
+    }),
 }));
